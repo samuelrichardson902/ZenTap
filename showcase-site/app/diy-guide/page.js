9a19dc4c41b471642f4e5cac7de2e91d5f509d47
Sam Richardson <samuelrichardson902@gmail.com>
2025-09-16 23:03:38 +0100

    fixed diy guide =escape chars

--- showcase-site/app/diy-guide/page.js
+++ showcase-site/app/diy-guide/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable react/no-unescaped-entities */
+
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import PhoneDisplay from "../components/PhoneDisplay";
